e2d04f02d7e9bd79c84d0ef412b35bb04734e4c4
Poppy Tait <poppytait@Poppys-MacBook-Air.local>
2018-10-06 14:11:11 +0200

    second iteration: turning the rover

--- javascripts/app.js
+++ javascripts/app.js
@@ -6,11 +6,59 @@ var rover = {
 // ======================
 function turnLeft(rover){
   console.log("turnLeft was called!");
+	switch (rover.direction) {
+		case "N":
+			rover.direction = "W"
+			break;
+
+		case "W":
+			rover.direction = "S"
+			break;
+
+		case "S":
+			rover.direction = "E"
+			break;
+
+		case "E":
+			rover.direction = "N"
+			break;
+
+		default:
+			rover.direction = "W"
+	}
+
 }
+
+turnLeft(rover);
+console.log(`The rover is now facing: ${rover.direction}`);
+
 
 function turnRight(rover){
   console.log("turnRight was called!");
+  switch (rover.direction) {
+		case "N":
+			rover.direction = "E"
+			break;
+
+		case "W":
+			rover.direction = "N"
+			break;
+
+		case "S":
+			rover.direction = "W"
+			break;
+
+		case "E":
+			rover.direction = "S"
+			break;
+
+		default:
+			rover.direction = "E"
+	}
 }
+
+turnRight(rover);
+console.log(`The rover is now facing: ${rover.direction}`);
 
 function moveForward(rover){
   console.log("moveForward was called")
